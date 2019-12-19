cfb87d995fe9d30fac3e8249e0f77b5596b2d228
Radoslaw Wiankowski <rwiankowski@mac.com>
2019-12-19 16:09:27 +0100

    Create installadds.ps1

---
+++ Module09/installadds.ps1
@@ -0,0 +1,20 @@
+$Password = Read-Host -Prompt 'Enter SafeMode Password' -AsSecureString
+
+Install-WindowsFeature -Name AD-Domain-Services
+
+$Params = @{
+    CreateDnsDelegation = $false
+    DatabasePath = 'C:\Windows\NTDS'
+    DomainMode = 'WinThreshold'
+    DomainName = 'az103.local'
+    DomainNetbiosName = 'az1o3local'
+    ForestMode = 'WinThreshold'
+    InstallDns = $true
+    LogPath = 'C:\Windows\NTDS'
+    NoRebootOnCompletion = $true
+    SafeModeAdministratorPassword = $Password
+    SysvolPath = 'C:\Windows\SYSVOL'
+    Force = $true
+}
+ 
+Install-ADDSForest @Params
